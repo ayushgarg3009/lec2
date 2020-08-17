7f86ea157ba2c1581bcaeb8e85810da564d2d229
Ayush <Ayush>
2020-08-17 17:22:43 +0530

    White-board

---
+++ lecture_14_UI_WhiteBoard/server/app.js
@@ -0,0 +1,30 @@
+// api 
+// const http = require("http");
+const express = require('express');
+const app = express();
+//  nodejs module
+const httpServer = require('http').createServer(app);
+//  socket enbaled server
+const io = require('socket.io')(httpServer);
+io.on("connection", function (socket) {
+    console.log("New client connected");
+    console.log(socket.id);
+    socket.on("color", function (color) {
+        // console.log(data);
+        socket.broadcast.emit('colorchange', color);
+    })
+    socket.on("md", function (point) {
+        socket.broadcast.emit("onmd", point);
+    })
+    socket.on("mm", function (point) {
+        socket.broadcast.emit("onmm", point);
+    })
+})
+
+// app.get("/home", function (req, res) {
+//     res.end("<h1>Welcome to home Page</h1>")
+// })
+//  connection
+httpServer.listen(3000, function () {
+    console.log("Server started at port 3000");
+})
